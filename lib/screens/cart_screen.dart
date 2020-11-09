a1e6b306e0de09f6458d5bb7d3df3cc9b9e01ac5
Ahsan <ahsan024@gmail.com>
2020-11-10 00:21:10 +0500

    cart screen basic

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_app/providers/cart.dart';
+import 'package:provider/provider.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = "/cart";
 
   @override
   Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+
     return Scaffold(
-      appBar: AppBar(),
-      body: Text("Cart"),
-    );
+        appBar: AppBar(
+          title: Text("Shopping Cart"),
+        ),
+        body: Column(
+          children: [
+            Card(
+              margin: EdgeInsets.all(15),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      "Total: ",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Spacer(),
+                    Chip(
+                      label: Text(
+                        "\$${cart.totalPrice}",
+                        style: TextStyle(
+                            color: Theme.of(context)
+                                .primaryTextTheme
+                                .headline6
+                                .color),
+                      ),
+                      backgroundColor: Theme.of(context).primaryColor,
+                    ),
+                    FlatButton(
+                      onPressed: () {},
+                      child: Text("ORDER NOW!"),
+                      textColor: Theme.of(context).primaryColor,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
