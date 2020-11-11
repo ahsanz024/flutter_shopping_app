0925cd62e81d41d80380311d0cecede2c08a46f2
Ahsan <ahsan024@gmail.com>
2020-11-11 21:47:51 +0500

    order prod list

--- lib/widgets/order_list_item.dart
+++ lib/widgets/order_list_item.dart
@@ -1,11 +1,20 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_shopping_app/providers/orders.dart';
 import 'package:intl/intl.dart';
 
-class OrderListItem extends StatelessWidget {
+class OrderListItem extends StatefulWidget {
   final OrderItem order;
 
   OrderListItem(this.order);
+
+  @override
+  _OrderListItemState createState() => _OrderListItemState();
+}
+
+class _OrderListItemState extends State<OrderListItem> {
+  bool _isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +25,44 @@ class OrderListItem extends StatelessWidget {
         child: Column(
           children: [
             ListTile(
-              title: Text("${this.order.price}"),
-              subtitle:
-                  Text("${DateFormat.yMEd().format(this.order.dateTime)}"),
+              title: Text(
+                "\$${this.widget.order.price}",
+                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+              ),
+              subtitle: Text(
+                  "${DateFormat.yMEd().format(this.widget.order.dateTime)}"),
               trailing: IconButton(
-                icon: Icon(Icons.expand_more),
-                onPressed: () {},
+                icon: Icon(_isExpanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _isExpanded = !_isExpanded;
+                  });
+                },
               ),
             ),
+            if (_isExpanded)
+              Container(
+                padding: EdgeInsets.all(10),
+                height: min(widget.order.products.length * 10 + 100.00, 180),
+                child: ListView(
+                  children: widget.order.products.map((p) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            p.title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700, fontSize: 15),
+                          ),
+                          Text("${p.quantity} x \$${p.price}")
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
           ],
         ),
       ),
